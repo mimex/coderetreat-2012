ede225432a4762a01cf208316efbced23b7c8a3b
mimex <mimex@fipasoft.mx>
2012-12-07 19:17:51 -0600

    Prueba unitaria en la suma de 2 enteros

--- Sum/SumTest.php
+++ Sum/SumTest.php
@@ -5,7 +5,20 @@
  * @author     mimeks <mimex@fipasoft.com.mx>
  * @license    http://www.gnu.org/licenses/gpl.html  GNU GPL
  */
+require './Sum.php';
 
 class SumTest extends PHPUnit_Framework_TestCase {
+	
+	public function testSumResult()
+	{
+		
+		$a = 1;
+		$b = 1;
+		
+		$s = new Sum( $a, $b );
+
+		$this->assertEquals( $s->result(), 2 );
+		
+	}
 	
 }?>
