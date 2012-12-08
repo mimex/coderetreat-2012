61e4f91c70cb71fd9ed2fc4cc7a79f02472453a8
mimex <mimex@fipasoft.mx>
2012-12-07 18:47:58 -0600

    Clase para pruebas unitarias con PHPUnit

---
+++ Hello/HelloTest.php
@@ -0,0 +1,14 @@
+<?php
+class HelloTest extends PHPUnit_Framework_TestCase
+{
+    
+    public function setUp(){
+    }
+    public function tearDown(){
+    }
+    public function testNothing()
+    {
+        $this->assertTrue( true );
+    }
+    
+}
