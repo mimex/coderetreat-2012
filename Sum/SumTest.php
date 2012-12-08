c1c8568a182c4c254d3eaf65b2be67a32034e730
mimex <mimex@fipasoft.mx>
2012-12-07 19:02:36 -0600

    Esqueleto de la suite de pruebas de la clase Sum

---
+++ Sum/SumTest.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * Clase para hacer pruebas de la clase Sum
+ *
+ * @author     mimeks <mimex@fipasoft.com.mx>
+ * @license    http://www.gnu.org/licenses/gpl.html  GNU GPL
+ */
+
+class SumTest extends PHPUnit_Framework_TestCase {
+	
+}?>
