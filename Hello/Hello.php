3c79ff3f4d5ccf132c881d25c3a2ba0eeedb52b2
mimex <mimex@fipasoft.mx>
2012-12-07 19:00:16 -0600

    Comentarios phpdoc

--- Hello/Hello.php
+++ Hello/Hello.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Clase para hacer un Hello World :D
+ * 
+ * @author     mimeks <mimex@fipasoft.com.mx>
+ * @license    http://www.gnu.org/licenses/gpl.html  GNU GPL
+ */
 Class Hello{
     
     public function Hello( $name = '' ){
--- Hello/HelloTest.php
+++ Hello/HelloTest.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Clase para hacer pruebas del Hello World
+ *
+ * @author     mimeks <mimex@fipasoft.com.mx>
+ * @license    http://www.gnu.org/licenses/gpl.html  GNU GPL
+ */
 class HelloTest extends PHPUnit_Framework_TestCase
 {
     
