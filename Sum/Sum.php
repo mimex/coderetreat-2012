03da02a278b4b802c67709be729f1297c9b8dd67
mimex <mimex@fipasoft.mx>
2012-12-07 19:02:19 -0600

    Esqueleto de la clase Sum

---
+++ Sum/Sum.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * Clase para hacer una suma de 2 enteros
+ *
+ * @author     mimeks <mimex@fipasoft.com.mx>
+ * @license    http://www.gnu.org/licenses/gpl.html  GNU GPL
+ */
+
+class Sum {
+	
+}?>
